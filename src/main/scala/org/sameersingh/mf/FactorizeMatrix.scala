7a0000bcd45bb249f4543275466467de184bae9e
Sameer Singh <sameeersingh@gmail.com>
2014-01-19 23:33:47 -0800

    update running example to bring everything together

--- src/main/scala/org/sameersingh/mf/FactorizeMatrix.scala
+++ src/main/scala/org/sameersingh/mf/FactorizeMatrix.scala
@@ -2,6 +2,7 @@ package org.sameersingh.mf
 
 import scala.util.Random
 import org.sameersingh.mf.learner.{Sampling, SGDTrainer}
+import org.sameersingh.utils.timing.TimeUtil
 
 /**
  * @author sameer
@@ -10,28 +11,47 @@ object FactorizeMatrix extends App {
   implicit val random = new Random(0)
   implicit val cache = new Cache
   val filename = "/Users/sameer/Work/data/mf/tac12.th3.profiles.gz"
+  val rOutputFilename = "/Users/sameer/Work/data/mf/mf_output/entity_profiles.gz"
+  val cOutputFilename = "/Users/sameer/Work/data/mf/mf_output/verb_profiles.gz"
+  TimeUtil.init
   val m = MatrixLoader.loadFile(filename, "M", 0.8, true)
-  println(m)
+  TimeUtil.snapshot(m.toString)
+
   val numComps = 100
+  val r = new DoubleDenseMatrix("r", numComps, () => random.nextGaussian() / 100.0)
+  val c = new DoubleDenseMatrix("c", numComps, () => random.nextGaussian() / 100.0)
+
   val params = new ParameterSet
-  params += new DoubleDenseMatrix("r", numComps, () => random.nextGaussian() / 100.0)
-  params += new DoubleDenseMatrix("c", numComps, () => random.nextGaussian() / 100.0)
-  params(params[DoubleDenseMatrix]("r"), "L2RegCoeff") = 0.1
-  params(params[DoubleDenseMatrix]("c"), "L2RegCoeff") = 0.1
-  val term = new DotTermWithBias(params, "r", "c", 1.0, m) with LogisticWithBias
+  params += r
+  params += c
+  params(r, "bias") = (() => random.nextGaussian() / 100.0)
+  params(c, "bias") = (() => random.nextGaussian() / 100.0)
+  params(r, "L2RegCoeff") = 0.1
+  params(c, "L2RegCoeff") = 0.1
+  // objective terms
+  val term = new DotTerm(params, "r", "c", 1.0, m) with Logistic
   val l2r = new L2Regularization(params, "r", m.trainCells.size)
   val l2c = new L2Regularization(params, "c", m.trainCells.size)
   val terms = Seq(term, l2r, l2c)
-
   val trainer = new SGDTrainer(Seq(m), terms) with Sampling
+
+  // evaluation terms
+  val nll = new DotTerm(params, "r", "c", 1.0, m) with Logistic
+  val l2Error = new DotTerm(params, "r", "c", 1.0, m) with L2
+  val binError = new DotTerm(params, "r", "c", 1.0, m) with BinaryError
+
   for (i <- 0 until 500) {
     println("-----------------------")
     println("        ROUND %d" format (i + 1))
     println("-----------------------")
-    println("Training value : " + term.avgTrainingValue(m))
-    println("Test value     : " + term.avgTestValue(m))
-    println("L2 R value     : " + l2r.value / l2r.weight())
-    println("L2 C value     : " + l2c.value / l2c.weight())
+    TimeUtil.snapshot("Training LL    : " + nll.avgTrainingValue(m))
+    TimeUtil.snapshot("Test LL        : " + nll.avgTestValue(m))
+    TimeUtil.snapshot("Training L2    : " + l2Error.avgTrainingValue(m))
+    TimeUtil.snapshot("Test L2        : " + l2Error.avgTestValue(m))
+    TimeUtil.snapshot("Training Err   : " + binError.avgTrainingValue(m))
+    TimeUtil.snapshot("Test Err       : " + binError.avgTestValue(m))
+    TimeUtil.snapshot("L2 R value     : " + l2r.value / l2r.weight())
+    TimeUtil.snapshot("L2 C value     : " + l2c.value / l2c.weight())
     //*/
     /*
     println("Row Factors:")
@@ -40,8 +60,20 @@ object FactorizeMatrix extends App {
     println(params("c"))
     */
     trainer.round(0.01)
+    TimeUtil.snapshot("Round done.")
+    DoubleDenseMatrix.save(r, rOutputFilename + (i%5), true)
+    DoubleDenseMatrix.save(c, cOutputFilename + (i%5), true)
+    TimeUtil.snapshot("Matrices written")
   }
-  println("Final Training value : " + term.avgTrainingValue(m))
-  println("Final Test value     : " + term.avgTestValue(m))
-
+  println("-----------------------")
+  println("        FINAL")
+  println("-----------------------")
+  TimeUtil.snapshot("Training LL    : " + nll.avgTrainingValue(m))
+  TimeUtil.snapshot("Test LL        : " + nll.avgTestValue(m))
+  TimeUtil.snapshot("Training L2    : " + l2Error.avgTrainingValue(m))
+  TimeUtil.snapshot("Test L2        : " + l2Error.avgTestValue(m))
+  TimeUtil.snapshot("Training Err   : " + binError.avgTrainingValue(m))
+  TimeUtil.snapshot("Test Err       : " + binError.avgTestValue(m))
+  TimeUtil.snapshot("L2 R value     : " + l2r.value / l2r.weight())
+  TimeUtil.snapshot("L2 C value     : " + l2c.value / l2c.weight())
 }
